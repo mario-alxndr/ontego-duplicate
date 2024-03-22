0984dd523037513ffb713e6754059e452d0ce9b7
mario-alxndr <marioalexander998@gmail.com>
2024-03-23 00:31:04 +0700

    feat: update something

--- components/Filter/index.tsx
+++ components/Filter/index.tsx
@@ -2,6 +2,7 @@
 
 // Node Modules
 import * as React from 'react';
+import cn from 'classnames';
 
 // Component
 import { Chip } from '../Chip';
@@ -11,6 +12,14 @@ import styles from './index.module.css';
 
 export const Filter = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleClickToggleFilter = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const additionalFilterClassnames = cn(styles.additional_filter, {
+    [styles.hide]: !isOpen
+  });
 
   return (
     <div className={styles.filter_container}>
@@ -32,48 +41,52 @@ export const Filter = () => {
           <Chip>Printer</Chip>
         </div>
       </div>
-      <div className={styles.filter_row}>
-        <div className={styles.filter_row_content}>
-          <p className={styles.filter_label}>Manufacturer</p>
-          <Chip>ACD</Chip>
-          <Chip>Advantech</Chip>
-          <Chip>Bixolone</Chip>
-          <Chip>Casio</Chip>
-          <Chip>Cat</Chip>
-          <Chip>Cipherlab</Chip>
-          <Chip>Datalogic</Chip>
-          <Chip>Cowardly</Chip>
-          <Chip>Getac</Chip>
-          <Chip>Gigaset</Chip>
-          <Chip>Honeywell</Chip>
-          <Chip>i.Safe Mobile</Chip>
-          <Chip>iData</Chip>
-          <Chip>Keyence</Chip>
-          <Chip>KoemTac</Chip>
-          <Chip>Newland</Chip>
-          <Chip>Panasonic</Chip>
-          <Chip>Point Mobile</Chip>
-          <Chip>ProGlobe</Chip>
-          <Chip>Samsung</Chip>
-          <Chip>zebra</Chip>
+      <div className={additionalFilterClassnames}>
+        <div className={styles.filter_row}>
+          <div className={styles.filter_row_content}>
+            <p className={styles.filter_label}>Manufacturer</p>
+            <Chip>ACD</Chip>
+            <Chip>Advantech</Chip>
+            <Chip>Bixolone</Chip>
+            <Chip>Casio</Chip>
+            <Chip>Cat</Chip>
+            <Chip>Cipherlab</Chip>
+            <Chip>Datalogic</Chip>
+            <Chip>Cowardly</Chip>
+            <Chip>Getac</Chip>
+            <Chip>Gigaset</Chip>
+            <Chip>Honeywell</Chip>
+            <Chip>i.Safe Mobile</Chip>
+            <Chip>iData</Chip>
+            <Chip>Keyence</Chip>
+            <Chip>KoemTac</Chip>
+            <Chip>Newland</Chip>
+            <Chip>Panasonic</Chip>
+            <Chip>Point Mobile</Chip>
+            <Chip>ProGlobe</Chip>
+            <Chip>Samsung</Chip>
+            <Chip>zebra</Chip>
+          </div>
         </div>
-      </div>
-      <div className={styles.filter_row}>
-        <div className={styles.filter_row_content}>
-          <p className={styles.filter_label}>operation area</p>
-          <Chip>Warehouse & Production</Chip>
-          <Chip>Beverage logistics</Chip>
-          <Chip>maintenance</Chip>
-          <Chip>Field Service</Chip>
-          <Chip>Healthcare</Chip>
-          <Chip>Explosion proof</Chip>
+        <div className={styles.filter_row}>
+          <div className={styles.filter_row_content}>
+            <p className={styles.filter_label}>operation area</p>
+            <Chip>Warehouse & Production</Chip>
+            <Chip>Beverage logistics</Chip>
+            <Chip>maintenance</Chip>
+            <Chip>Field Service</Chip>
+            <Chip>Healthcare</Chip>
+            <Chip>Explosion proof</Chip>
+          </div>
         </div>
       </div>
       <div className={styles.filter_bottom}>
         <p className={styles.filter_caption}>All information/prices without guarantee</p>
         <div>
           <p className={styles.reset_button}>reset filter</p>
-          <p className={styles.toggle_button}>{isOpen ? 'Hide filters' : 'Show more filters'}</p>
+          <p className={styles.toggle_button} onClick={handleClickToggleFilter}>
+            {isOpen ? 'Hide filters' : 'Show more filters'}
+          </p>
         </div>
       </div>
     </div>
